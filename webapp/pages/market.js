780662d9c73317667624ef61c9e7ca6c2d2e7724
Nitish K S <[nitishsgk@gmail.com]>
2023-01-21 16:10:59 +0530

    added Search Engine

--- webapp/pages/market.js
+++ webapp/pages/market.js
@@ -1,11 +1,12 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import style from "./../styles/market.module.scss"
 import Loading from "../comps/loading"
 import StockDashboard from "../comps/StockDashboard";
 import BuyPortal from "../comps/buyPortal";
 import { Raleway } from "@next/font/google";
 import useSWR from 'swr';
+import tickerList from '../db/tableConvert.com_gajv7t.json'
 
 const fetcher = async () => {
 	const res = await axios.get(`https://api.exchangerate-api.com/v4/latest/INR`);
@@ -26,14 +27,27 @@ const Market = () => {
 	const [buyPortal, setBuyPortal] = useState(false);
 	const [errMsg, setErrMsg] = useState("");
 	const { data: currRates } = useSWR('currencyConverter', fetcher)
+	const [symbolList, setSymbolList] = useState([]);
+	const [showSearchList, setShowSearchList] = useState(false);
+	const [searchVal, setSearchVal] = useState("");
 
 	const convertToINR = (amt, currency) => {
-		console.log(amt,currency);
+		console.log(amt, currency);
 		return (amt / currRates.rates[currency]).toFixed(2);
 	}
 
 	const handleChange = (e) => {
 		setSymbol(e.target.value);
+		setShowSearchList(true)
+		setShowSearchList(true)
+		setSearchVal(e.target.value)
+		if (e.target.value.trim().length > 1) {
+			setSymbolList(tickerList.filter((curr) => {
+				const regex = new RegExp('^' + e.target.value + '.*', 'i')
+				return regex.test(curr.Ticker)
+			}))
+			console.log(symbolList)
+		}
 	}
 	const handleSubmit = async (e) => {
 		if (e.keyCode == 13) {
@@ -56,20 +70,37 @@ const Market = () => {
 	return (
 		<section className={style.searchSection}>
 			<p className={raleway.className}> Search for stocks </p>
-			<input className={style.searchbar} onChange={handleChange} onKeyDown={handleSubmit} type="text" name="symbol" placeholder="Type a Company or Brand symbol to search" autoFocus />
-			{loading &&
+			<div className={style.searchBarContainer}>
+				<input className={style.searchbar} value={symbol} onBlur={() => setShowSearchList(false)} onChange={handleChange} onKeyDown={handleSubmit} type="text" name="symbol" placeholder="Type a Company or Brand symbol to search" autoFocus />
+				{showSearchList &&
+					<ul className={style.datalist}>
+						{searchVal != "" && <li>{searchVal}</li>}
+						{
+							symbolList.map((ticker, key) => (
+								<li key={key} onMouseOver={(e) => setSymbol(ticker.Ticker)} >
+									{ticker.Ticker}-{ticker.Name}
+								</li>
+							))
+						}
+					</ul>
+				}
+			</div>
+			{
+				loading &&
 				<Loading />
 			}
 			{
 				<p className={style.err}>{errMsg}</p>
 			}
-			{stock &&
+			{
+				stock &&
 				<StockDashboard stock={stock} setBuyPortal={setBuyPortal} convertToINR={convertToINR} />
 			}
-			{buyPortal &&
+			{
+				buyPortal &&
 				<BuyPortal stock={stock} setBuyPortal={setBuyPortal} convertToINR={convertToINR} />
 			}
-		</section>
+		</section >
 	)
 }
 
